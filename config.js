0ede1b87da4db0fe9ccd3ba86059abc3f1aaa2d6
Rikesh Dahal <85576731+rikeshdahal@users.noreply.github.com>
2024-05-31 17:23:24 +0545

    Add files via upload

---
+++ config.js
@@ -0,0 +1,183 @@
+
+const config = {
+
+    "help": [
+        {
+            "title": "help",
+            "description": "for a list of commands(add flags '-d' for commands description)",
+            "info": ["help or ls command gives the list of commands","add '-d' flag for description of all commands also","write command after help to get info about that specific command like 'help typing'"]
+        },
+        {
+            "title": "clear",
+            "description": "to clear the terminal",
+            "info": ["clear or cls command clears the terminal"]
+        },
+        {
+            "title": "about",
+            "description": "to learn more about me",
+            "info": ["use about command to learn more about me"]
+        },
+        {
+            "title": "social",
+            "description": "to see my social links (add flags '-l' for links and '-d' for detailed results)",
+            "info": ["use social command to see my social links","add '-l' flag for links and '-d' flag for detailed results"]
+        },
+        {
+            "title": "projects",
+            "description": "to see my projects",
+            "info": ["use projects command to see my projects"]
+        },
+        {
+            "title": "blogs",
+            "description": "to see my recent blogs",
+            "info": ["use blogs command to see my recent blogs"]
+        },
+        {
+            "title": "contact",
+            "description": "to enquire about my services",
+            "info": ["use enquire command to enquire about my services"]
+        },
+        {
+            "title": "cheer",
+            "description": "to appreciate my work",
+            "info": ["use cheer command to appreciate my work"]
+        },
+        {
+            "title": "repos",
+            "description": "to see my github repositories",
+            "info": ["use repos command to see my github repositories"]
+        },
+        {
+            "title": "ipconfig",
+            "description": "to see your IP details",
+            "info": ["use ipconfig command to see your IP details"]
+        },
+        {
+            "title": "github",
+            "description": "to see my github stats",
+            "info": ["use github command to see my github stats"]
+        },
+        {
+            "title": "contributors",
+            "description": "to see all the contributors",
+            "info": ["use contributors command to see all the contributors"]
+        },
+        {
+            "title": "download",
+            "description": "to download my pdf resume",
+            "info": ["use download command to download my pdf resume"]
+        },
+        {
+            "title": "calc",
+            "description": "to evaluate an expression, for eg: (2 + 3)",
+            "info": ["use calc command to evaluate an expression","for eg: write 'calc 2+6*5/3'"]
+        },
+        {
+            "title": "experience",
+            "description": "to see my work experience",
+            "info": ["use experience command to see my work experience"]
+        },
+        {
+            "title": "history",
+            "description": "shows the last 10 valid commands performed, use --clear flag to clear the history",
+            "info": ["use history command to show your last 10 commands history","use --clear flag to clear the history","use history {id} command to run command of that id in your history"]
+        },
+        {
+            "title": "skills",
+            "description": "to see my skills",
+            "info": ["use skills command to see my skills"]
+        },
+        {
+            "title": "typing",
+            "description": "shows typing animation status",
+            "info": ["use typing command to see typing animation status","Turn typing animation on and off by adding -on or -off flags respectively","Also u can write a number(in ms) to set typing custom animation speed"]
+        },
+        {
+            "title": "reset",
+            "description": "to reload site",
+            "info": ["use reset command to to reload site"]
+        }
+    ],
+    "terminal": {
+        "user": "$ Rikesh_Dahal👻",
+        "host": "<sudo",
+        "path": "~/guest>"
+    },
+    "blogs": [
+        {
+            "site": "Website",
+            "url": "https://rikeshdahal.com.np"
+        }
+    ],
+    "cheer": {
+        "responseArray": [
+            "Thank you! It makes my day😊😊😊",
+            "It is great to hear that way!😁😁😁",
+            "I would love to take credit😂😂😂",
+            "That's so good to hear! I'm glad😍😍😍"
+        ]
+    },
+    "about": "🎓 Aspiring software engineer with strong foundation in computer science and engineering, gained through BE degree program and hands-on experience with various technologies. (Graduating July 2024) \n 🌇 Proficient in MERN stack, data structures and algorithms, SQL, and Google Cloud Platform. Demonstrated expertise in these technologies through successful projects and 300+ day badge on LeetCode. \n 📚 Active member of tech community, serving as lead for multiple organizations including GDSC, CodeChef Chapter, and Algoders Community. Contributed to growth and success of these organizations through leadership and technical skills. \n 💻 Constantly seeking opportunities to grow and improve skills, with long-term goal of becoming a software development engineer. As a Google Venkat Scholar, demonstrated commitment to excellence and potential to succeed in tech industry.",
+    "social": [
+        {
+            "title": "Github",
+            "link": "https://github.com/rikeshdahal"
+        },
+        {
+            "title": "LinkedIn",
+            "link": "https://www.linkedin.com/in/rikeshdahal",
+            "connections": "50+"
+        },
+        {
+            "title": "tiktok",
+            "link": "https://www.tiktok.com/mrmadhacker",
+            "followera": "500+"
+        },
+
+    ],
+    "projects": [
+    
+        {
+            "title": "Ripple",
+            "link": "https://github.com/rikeshdahal/ripple",
+            "description": "The website that lets you add a unique touch to your user experience! With ripple, you can create a ripple effect on mouse hover that adds a visually engaging element to your website. Whether you're looking to add a subtle touch of interactivity or want to create eye-catching effect, Ripple is the perfect for you. So why wait? Head over to our website and start creating your own ripples today!"
+        },
+        {
+            "title": "Window Updating Prank Website",
+            "link": "https://github.com/rikeshdahal/Window Updating Prank Website",
+            "description": "With just a few clicks, you can create a fake Windows update screen that will trick your friends and family into thinking their computer is undergoing a critical update. Watch as they panic and scramble to save their work, only to realize that they've been pranked."
+        },
+        {
+            "title": "Touch Typing practice Keyboards",
+            "link": "https://github.com/rikeshdahal/Touch Typing practice Keyboards",
+            "description": "The website that helps you improve your typing skills and increase your speed and accuracy! With our easy-to-use platform, you can practice touch typing on the keyboard and master the art of typing without looking at the keys. So if you're ready to take your typing skills to the next level, head over to our Touch Typing Keyboard website and start practicing today!"
+        }
+    ],
+    "contact": {
+        "email": "rikeshhacker747@gmail.com"
+    },
+    "experience": [
+        {
+            "title": "Unity Game Development",
+            "description": "▪️3 years of game development experence."
+        }
+    ],
+    "skills": [
+        {
+            "title": "Technical skills are: ",
+            "description": "Pyhton, C, C#, MySQL, PHP, Linux, UI/UX  "
+        },
+        {
+            "title": "Other Skills are:",
+            "description": "Problem Solving, Team Leadership, Community Management, Team Building"
+        }
+    ],
+    "neofetch": {
+        "name": "Rikesh Dahal",
+        "title": "Ethical Hacker & Game Developer",
+        "skills": "Frontend, Backend, Cloud",
+        "shell": "zsh",
+        "languages": "Python, Javascript, C, C#, HTML/CSS, SQL"
+    },
+}
+export default config
